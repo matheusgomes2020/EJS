d894c84af558a8286c5b4e7a1481a82f84873251
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-04-20 16:18:43 -0300

    initial commit

---
+++ date.js
@@ -0,0 +1,31 @@
+//jshint esversion:6
+
+exports.getDate = getDate = function() {
+
+    const today = new Date();
+
+    const options = {
+
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+
+    };
+
+    return today.toLocaleDateString("en-US", options);
+
+}
+
+exports.getDay = function() {
+
+    const today = new Date();
+
+    const options = {
+
+        weekday: "long"
+
+    };
+
+    return today.toLocaleDateString("en-US", options);
+
+}
